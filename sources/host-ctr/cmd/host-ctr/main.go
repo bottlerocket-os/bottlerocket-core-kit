6ff7adde091b019e36a0ae0e2072e7f456595ccb
Patrick J.P. Culp <jpculp@amazon.com>
2024-07-31 20:25:17 +0000

    host-ctr: remove ca-west-1 as a special region

--- sources/host-ctr/cmd/host-ctr/main.go
+++ sources/host-ctr/cmd/host-ctr/main.go
@@ -597,7 +597,6 @@ func cleanUp(containerdSocket string, namespace string, containerID string) erro
 // Referenced source: https://github.com/awslabs/amazon-ecr-containerd-resolver/blob/a5058cf091f4fc573813a032db37a9820952f1f9/ecr/ref.go#L70-L71
 func parseImageURISpecialRegions(input string) (ecr.ECRSpec, error) {
 	ecrRefPrefixMapping := map[string]string{
-		"ca-west-1": "ecr.aws/arn:aws:ecr:ca-west-1:",
 		"ap-southeast-5": "ecr.aws/arn:aws:ecr:ap-southeast-5:",
 	}
 	// Matching on account, region
