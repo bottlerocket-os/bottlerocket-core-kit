7d0ae4730ee99007e01898e9bfa42ec6c149efb8
Dhwani Serai <dhwanise@amazon.com>
2025-10-24 23:45:04 +0000

    tools: add additional version checks to changelog validation
    Signed-off-by: Dhwani Serai <dhwanise@amazon.com>

--- tools/validate-changelog.sh
+++ tools/validate-changelog.sh
@@ -4,9 +4,32 @@
 # this should be sufficient to validate the CHANGELOG for our CI, provided that
 # every new tag has a corresponding CHANGELOG update, as we always parse the
 # CHANGELOG between two headers with a tagged version.
-if diff <(grep -ne '^# ' CHANGELOG.md) <(grep -ne '^# v[0-9]\+\.[0-9]\+\.[0-9]\+' CHANGELOG.md); then
-  echo "CHANGELOG validation PASSED!"
-else
+if ! diff <(grep -ne '^# ' CHANGELOG.md) <(grep -ne '^# v[0-9]\+\.[0-9]\+\.[0-9]\+' CHANGELOG.md); then
   echo "CHANGELOG validation FAILED! Headers must match the regex '^# v[0-9]\+\.[0-9]\+\.[0-9]\+.'"
   exit 1
 fi
+
+# check that all versions are unique
+versions=$(grep -o '^# v[0-9]\+\.[0-9]\+\.[0-9]\+' CHANGELOG.md)
+duplicates=$(echo "${versions}" | sort | uniq -d)
+if [[ -n "${duplicates}" ]]; then
+  echo "CHANGELOG validation FAILED! Duplicate versions found:"
+  echo "${duplicates}"
+  exit 1
+fi
+
+# check that versions are in descending order
+# sort the versions and check the diff with current set of versions
+sorted_versions=$(echo "${versions}" | sort -V -r)
+if ! diff <(echo "${sorted_versions}") <(echo "${versions}") > /dev/null; then
+  echo "CHANGELOG validation FAILED! Versions must be in descending order."
+  paste <(echo "${versions}") <(echo "${sorted_versions}") | while IFS=$'\t' read -r actual expected; do
+    if [[ "${actual}" != "${expected}" ]]; then
+      echo "Found ${actual} but expected ${expected}"
+      break
+    fi
+  done
+  exit 1
+fi
+
+echo "CHANGELOG validation PASSED!"
