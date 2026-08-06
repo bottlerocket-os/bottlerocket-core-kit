21364f8c104f5f2883663fce2716af56ddd36eb0
Vighnesh Maheshwari <vighmah@amazon.com>
2026-08-06 18:08:51 +0000

    packages: update libcrypto to 3.3.0
    Update the aws-lc source to the AWS-LC-FIPS-3.3.0 tag and regenerate
    upstream patches from the fips-2024-09-27 branch.
    
    Signed-off-by: Vighnesh Maheshwari <vighmah@amazon.com>

--- packages/libcrypto/generate-aws-lc-patches.sh
+++ packages/libcrypto/generate-aws-lc-patches.sh
@@ -4,4 +4,4 @@ git clone https://github.com/aws/aws-lc
 cd aws-lc
 git checkout origin/fips-2024-09-27
 git format-patch --start-number=1019 --no-numbered --no-signature \
-    AWS-LC-FIPS-3.1.0..
+    AWS-LC-FIPS-3.3.0..
